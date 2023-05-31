fb6af871238b4d20601874f104a083c2743d95ba
unbanksytv <unbanksytv@gmail.com>
2023-05-31 10:04:02 +0200

    new test contracts made by GPT 4.20

--- contracts/VagaBonds.sol
+++ contracts/VagaBonds.sol
@@ -1,100 +1,182 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "./IERC20.sol";
+import "./SafeMath.sol";
 
-contract IkigaiNFT is ERC721, Ownable {
-    IUniswapV2Router02 public uniswapRouter;
-    IUniswapV2Pair public liquidityPair;
-    address public WETH;
+contract NFTMarketplace {
+    using SafeMath for uint256;
 
-    uint256 public constant MINT_PRICE = 1 ether;
-    uint256 public constant MINT_LIMIT = 10000;
+    // Contract variables
+    uint256 private constant MAX_SUPPLY = 10000;
+    uint256 private constant LOCK_DURATION = 30 days;
+    uint256 private constant EMERGENCY_STOP_DURATION = 7 days;
 
-    uint256 public totalMinted;
-    uint256 public lockedLiquidity;
+    // Token variables
+    IERC20 private token;
+    address private nftContract;
+    address private uniswapPair;
 
-    struct Proposal {
-        uint256 amount;
-        uint256 endTime;
-        uint256 yesVotes;
-        mapping(address => bool) hasVoted;
+    // Liquidity variables
+    uint256 private liquidityTokens;
+    uint256 private liquidityETH;
+    uint256 private liquidityLockExpiration;
+
+    // Voting variables
+    uint256 private votingDuration;
+    uint256 private votingLockExpiration;
+    mapping(address => uint256) private votingPower;
+    mapping(address => mapping(uint256 => bool)) private hasVoted;
+
+    // Emergency stop variables
+    bool private emergencyStop;
+    uint256 private emergencyStopExpiration;
+
+    // Events
+    event TokensMinted(address indexed recipient, uint256 amount);
+    event TokensBurned(address indexed holder, uint256 amount);
+    event LiquidityAdded(address indexed provider, uint256 amountETH, uint256 amountTokens);
+    event LiquidityRemoved(address indexed provider, uint256 amountETH, uint256 amountTokens);
+    event VotingPowerUpdated(address indexed holder, uint256 votingPower);
+    event ProposalCreated(uint256 proposalId, address indexed creator, string proposalData);
+    event VoteCasted(uint256 proposalId, address indexed voter, bool vote);
+    event ProposalExecuted(uint256 proposalId, address indexed executor);
+
+    // Modifier to check if the contract is not in emergency stop mode
+    modifier notInEmergencyStop() {
+        require(!emergencyStop, "Contract is in emergency stop mode");
+        _;
     }
 
-    Proposal[] public proposals;
-
-    constructor(address _uniswapRouter) ERC721("Ikigai NFT", "IKG") {
-        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
-        WETH = uniswapRouter.WETH();
+    // Modifier to check if the contract is not in emergency stop and emergency stop duration has passed
+    modifier emergencyStopExpired() {
+        require(!emergencyStop || block.timestamp > emergencyStopExpiration, "Contract is in emergency stop mode");
+        _;
     }
 
-    function mint(uint256 amount) external payable {
-        require(totalSupply() + amount <= MINT_LIMIT, "Mint limit reached");
-        require(msg.value >= MINT_PRICE * amount, "Ether value sent is not correct");
+    // Modifier to check if the sender has voting power
+    modifier hasVotingPower() {
+        require(votingPower[msg.sender] > 0, "Sender does not have voting power");
+        _;
+    }
 
-        uint256 liquidityAmount = (msg.value * 80) / 100;
-        lockedLiquidity += liquidityAmount;
+    constructor(address _token, address _nftContract, address _uniswapPair) {
+        token = IERC20(_token);
+        nftContract = _nftContract;
+        uniswapPair = _uniswapPair;
+        liquidityLockExpiration = block.timestamp.add(LOCK_DURATION);
+        votingDuration = 7 days;
+        votingLockExpiration = block.timestamp.add(votingDuration);
+    }
 
-        for (uint256 i = 0; i < amount; i++) {
-            _mint(msg.sender, totalMinted + i);
+    // Function to mint tokens
+    function mintTokens(address recipient, uint256 amount) external notInEmergencyStop {
+        require(totalSupply().add(amount) <= MAX_SUPPLY, "Maximum supply reached");
+        _mint(recipient, amount);
+        emit TokensMinted(recipient, amount);
+    }
+
+    // Function to burn tokens
+    function burnTokens(uint256 amount) external notInEmergencyStop {
+        _burn(msg.sender, amount);
+        emit TokensBurned(msg.sender, amount);
+    }
+
+    // Function to add liquidity to the contract
+    function addLiquidity(uint256 amountETH, uint256 amountTokens) external payable notInEmergencyStop {
+        require(msg.value == amountETH, "Incorrect ETH amount sent");
+        require(token.transferFrom(msg.sender, address(this), amountTokens), "Token transfer failed");
+
+        liquidityETH = liquidityETH.add(amountETH);
+        liquidityTokens = liquidityTokens.add(amountTokens);
+        emit LiquidityAdded(msg.sender, amountETH, amountTokens);
+
+        // Lock the liquidity if it's the first time adding liquidity or the previous lock has expired
+        if (liquidityLockExpiration == 0 || block.timestamp > liquidityLockExpiration) {
+            liquidityLockExpiration = block.timestamp.add(LOCK_DURATION);
         }
-
-        totalMinted += amount;
     }
 
-    function createProposal(uint256 amount) external onlyOwner {
-        proposals.push(Proposal({
-            amount: amount,
-            endTime: block.timestamp + 7 days, // Proposal expires after one week
-            yesVotes: 0
-        }));
+    // Function to remove liquidity from the contract
+    function removeLiquidity(uint256 amountETH, uint256 amountTokens) external notInEmergencyStop {
+        require(block.timestamp > liquidityLockExpiration, "Liquidity is locked");
+
+        require(amountETH <= liquidityETH, "Insufficient ETH liquidity");
+        require(amountTokens <= liquidityTokens, "Insufficient token liquidity");
+
+        liquidityETH = liquidityETH.sub(amountETH);
+        liquidityTokens = liquidityTokens.sub(amountTokens);
+        emit LiquidityRemoved(msg.sender, amountETH, amountTokens);
+
+        require(token.transfer(msg.sender, amountTokens), "Token transfer failed");
+        payable(msg.sender).transfer(amountETH);
     }
 
-    function vote(uint256 proposalIndex, bool vote) external {
-        Proposal storage proposal = proposals[proposalIndex];
+    // Function to update the voting power of a holder
+    function updateVotingPower(address holder, uint256 votingPower_) external {
+        require(msg.sender == holder || votingPower[msg.sender] > 0, "Sender does not have permission");
 
-        require(!proposal.hasVoted[msg.sender], "Already voted");
-        require(block.timestamp <= proposal.endTime, "Proposal expired");
-
-        proposal.hasVoted[msg.sender] = true;
-
-        if (vote) {
-            proposal.yesVotes += balanceOf(msg.sender);
-        }
+        votingPower[holder] = votingPower_;
+        emit VotingPowerUpdated(holder, votingPower_);
     }
 
-    function executeProposal(uint256 proposalIndex) external onlyOwner {
-        Proposal storage proposal = proposals[proposalIndex];
+    // Function to create a proposal
+    function createProposal(string calldata proposalData) external hasVotingPower {
+        require(bytes(proposalData).length > 0, "Proposal data is empty");
 
-        require(block.timestamp > proposal.endTime, "Proposal not yet expired");
-        require(proposal.yesVotes * 2 > totalSupply(), "Not enough votes"); // Requires majority
+        uint256 proposalId = uint256(keccak256(abi.encodePacked(msg.sender, proposalData, block.timestamp)));
 
-        removeLiquidity(proposal.amount);
+        // Store proposal details
+
+        emit ProposalCreated(proposalId, msg.sender, proposalData);
     }
 
-    function removeLiquidity(uint256 liquidity) internal {
-        require(liquidityPair.balanceOf(address(this)) >= liquidity, "Not enough LP tokens");
+    // Function to cast a vote for a proposal
+    function castVote(uint256 proposalId, bool vote) external hasVotingPower {
+        require(!hasVoted[msg.sender][proposalId], "Already voted for this proposal");
 
-        // Approve Uniswap router to transfer LP tokens
-        liquidityPair.approve(address(uniswapRouter), liquidity);
+        // Cast the vote
 
-        // Remove the liquidity
-        (uint256 amountToken, uint256 amountETH) = uniswapRouter.removeLiquidityETH(
-            address(this),
-            WETH,
-            liquidity,
-            0, // slippage is unavoidable
-            0, // slippage is unavoidable
-            address(this),
-            block.timestamp
-        );
+        hasVoted[msg.sender][proposalId] = true;
+        emit VoteCasted(proposalId, msg.sender, vote);
+    }
 
-        // Redistribute the received funds among the token holders
-        for (uint256 i = 0; i < totalSupply(); i++) {
-            payable(ownerOf(i)).transfer((amountETH * balanceOf(ownerOf(i))) / totalSupply());
-            _safeTransfer(ownerOf(i), ownerOf(i), amountToken * balanceOf(ownerOf(i)) / totalSupply(), "");
-        }
+    // Function to execute a proposal
+    function executeProposal(uint256 proposalId) external {
+        require(hasVoted[msg.sender][proposalId], "Sender has not voted for this proposal");
+
+        // Execute the proposal
+
+        emit ProposalExecuted(proposalId, msg.sender);
+    }
+
+    // Function to get the total supply of tokens
+    function totalSupply() public view returns (uint256) {
+        // Return the total supply
+    }
+
+    // Function to get the balance of an account
+    function balanceOf(address account) public view returns (uint256) {
+        // Return the balance of the account
+    }
+
+    // Internal function to mint tokens
+    function _mint(address account, uint256 amount) internal {
+        // Mint the tokens
+    }
+
+    // Internal function to burn tokens
+    function _burn(address account, uint256 amount) internal {
+        // Burn the tokens
+    }
+
+    // Function to handle ETH received
+    receive() external payable {
+        // Handle ETH received
+    }
+
+    // Function to handle ETH sent accidentally without a function call
+    fallback() external payable {
+        // Handle ETH sent accidentally
     }
 }
